0e422c1a637d9217f2493f06c0535a6464112117
Dario Benitez <benitezdario007@gmail.com>
2025-11-22 09:04:09 -0300

    Eliminar carpetas innecesarias

--- frontend/src/pages/Confirmation.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Confirmation() {
-  return <div><h2>Gracias por tu compra</h2><p>Si usaste el modo test, el pago estará en estado de prueba.</p></div>;
-}
